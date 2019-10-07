de56fa813214f06b428828fc0d7f78043b71ffbf
Ross Light <ross@zombiezen.com>
2019-10-07 09:05:11 -0700

    graphqlhttp: add functions from bass

---
+++ graphqlhttp/graphqlhttp.go
@@ -0,0 +1,143 @@
+// Copyright 2019 Ross Light
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// Package graphqlhttp provides functions for serving GraphQL over HTTP as
+// described in https://graphql.org/learn/serving-over-http/.
+package graphqlhttp
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"mime"
+	"net/http"
+	"strconv"
+
+	"golang.org/x/xerrors"
+	"zombiezen.com/go/graphql-server/graphql"
+)
+
+// Parse parses a GraphQL HTTP request. If an error is returned, StatusCode
+// will return the proper HTTP status code to use.
+//
+// Request methods may be GET, HEAD, or POST. If the method is not one of these,
+// then an error is returned that will make StatusCode return
+// http.StatusMethodNotAllowed.
+func Parse(r *http.Request) (graphql.Request, error) {
+	request := graphql.Request{
+		Query: r.URL.Query().Get("query"),
+	}
+	switch r.Method {
+	case http.MethodGet, http.MethodHead:
+		if v := r.FormValue("variables"); v != "" {
+			if err := json.Unmarshal([]byte(v), &request.Variables); err != nil {
+				return graphql.Request{}, &httpError{
+					msg:   "parse graphql request: ",
+					code:  http.StatusBadRequest,
+					cause: err,
+				}
+			}
+		}
+		request.OperationName = r.FormValue("operationName")
+	case http.MethodPost:
+		rawContentType := r.Header.Get("Content-Type")
+		contentType, _, err := mime.ParseMediaType(rawContentType)
+		if err != nil {
+			return graphql.Request{}, &httpError{
+				msg:  "parse graphql request: invalid content type: " + rawContentType,
+				code: http.StatusUnsupportedMediaType,
+			}
+		}
+		switch contentType {
+		case "application/json":
+			if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+				return graphql.Request{}, &httpError{
+					msg:   "parse graphql request: ",
+					code:  http.StatusBadRequest,
+					cause: err,
+				}
+			}
+		case "application/x-www-form-urlencoded":
+			request.Query = r.FormValue("query")
+		case "application/graphql":
+			data, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				return graphql.Request{}, &httpError{
+					msg:   "parse graphql request: ",
+					code:  http.StatusBadRequest,
+					cause: err,
+				}
+			}
+			if len(data) > 0 {
+				request.Query = string(data)
+			}
+		default:
+			return graphql.Request{}, &httpError{
+				msg:  "parse graphql request: unrecognized content type: " + contentType,
+				code: http.StatusUnsupportedMediaType,
+			}
+		}
+	default:
+		return graphql.Request{}, &httpError{
+			msg:  fmt.Sprintf("parse graphql request: method %s not allowed", r.Method),
+			code: http.StatusMethodNotAllowed,
+		}
+	}
+	return request, nil
+}
+
+type httpError struct {
+	msg   string
+	code  int
+	cause error
+}
+
+func (e *httpError) Error() string {
+	if e.cause == nil {
+		return e.msg
+	}
+	return e.msg + e.cause.Error()
+}
+
+func (e *httpError) Unwrap() error {
+	return e.cause
+}
+
+// StatusCode returns the HTTP status code an error indicates.
+func StatusCode(err error) int {
+	if err == nil {
+		return http.StatusOK
+	}
+	var e *httpError
+	if !xerrors.As(err, &e) {
+		return http.StatusInternalServerError
+	}
+	return e.code
+}
+
+// WriteResponse writes a GraphQL result as an HTTP response.
+func WriteResponse(w http.ResponseWriter, result graphql.Response) {
+	payload, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, "GraphQL marshal error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Content-Length", strconv.Itoa(len(payload)))
+	if _, err := w.Write(payload); err != nil {
+		return
+	}
+}
---
+++ graphqlhttp/graphqlhttp_test.go
@@ -0,0 +1,167 @@
+// Copyright 2019 Ross Light
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package graphqlhttp
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"zombiezen.com/go/graphql-server/graphql"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		name string
+
+		method      string
+		query       url.Values
+		contentType string
+		body        string
+
+		want          graphql.Request
+		wantErrStatus int
+	}{
+		{
+			name:   "HEAD",
+			method: http.MethodHead,
+			query:  url.Values{"query": {"{me{name}}"}},
+			want: graphql.Request{
+				Query: "{me{name}}",
+			},
+		},
+		{
+			name:   "GET/JustQuery",
+			method: http.MethodGet,
+			query:  url.Values{"query": {"{me{name}}"}},
+			want: graphql.Request{
+				Query: "{me{name}}",
+			},
+		},
+		{
+			name:   "GET/AllFields",
+			method: http.MethodGet,
+			query: url.Values{
+				"query":         {"{me{name}}"},
+				"variables":     {`{"foo":"bar"}`},
+				"operationName": {"Baz"},
+			},
+			want: graphql.Request{
+				Query:         "{me{name}}",
+				OperationName: "Baz",
+				Variables: map[string]graphql.Input{
+					"foo": graphql.ScalarInput("bar"),
+				},
+			},
+		},
+		{
+			name:        "POST/JustQuery",
+			method:      http.MethodPost,
+			contentType: "application/json; charset=utf-8",
+			body:        `{"query": "{me{name}}"}`,
+			want: graphql.Request{
+				Query: "{me{name}}",
+			},
+		},
+		{
+			name:        "POST/AllFields",
+			method:      http.MethodPost,
+			contentType: "application/json; charset=utf-8",
+			body:        `{"query": "{me{name}}", "variables": {"foo":"bar"}, "operationName": "Baz"}`,
+			want: graphql.Request{
+				Query:         "{me{name}}",
+				OperationName: "Baz",
+				Variables: map[string]graphql.Input{
+					"foo": graphql.ScalarInput("bar"),
+				},
+			},
+		},
+		{
+			name:        "POST/QueryInURL",
+			method:      http.MethodPost,
+			query:       url.Values{"query": {"{me{name}}"}},
+			contentType: "application/json; charset=utf-8",
+			body:        `{"variables": {"foo":"bar"}, "operationName": "Baz"}`,
+			want: graphql.Request{
+				Query:         "{me{name}}",
+				OperationName: "Baz",
+				Variables: map[string]graphql.Input{
+					"foo": graphql.ScalarInput("bar"),
+				},
+			},
+		},
+		{
+			name:        "POST/QueryInBodyAndURL",
+			method:      http.MethodPost,
+			query:       url.Values{"query": {"{me{name}}"}},
+			contentType: "application/json; charset=utf-8",
+			body:        `{"query": "{your{face}}", "variables": {"foo":"bar"}, "operationName": "Baz"}`,
+			want: graphql.Request{
+				Query:         "{your{face}}",
+				OperationName: "Baz",
+				Variables: map[string]graphql.Input{
+					"foo": graphql.ScalarInput("bar"),
+				},
+			},
+		},
+		{
+			name:        "POST/GraphQLContentType",
+			method:      http.MethodPost,
+			contentType: "application/graphql; charset=utf-8",
+			body:        "{me{name}}",
+			want: graphql.Request{
+				Query: "{me{name}}",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req := &http.Request{
+				Method: test.method,
+				URL: &url.URL{
+					RawQuery: test.query.Encode(),
+				},
+				Header: make(http.Header),
+				Body:   ioutil.NopCloser(strings.NewReader(test.body)),
+			}
+			if test.contentType != "" {
+				req.Header.Set("Content-Type", test.contentType)
+			}
+			got, err := Parse(req)
+			if err != nil {
+				if test.wantErrStatus == 0 {
+					t.Fatalf("Parse error = %v; want <nil>", err)
+				}
+				if StatusCode(err) != test.wantErrStatus {
+					t.Fatalf("Parse error = %v, status code = %d; want status code = %d", err, StatusCode(err), test.wantErrStatus)
+				}
+				return
+			}
+			if test.wantErrStatus != 0 {
+				t.Fatalf("Parse(...) = %+v, <nil>; want error status code = %d", got, test.wantErrStatus)
+			}
+			diff := cmp.Diff(test.want, got,
+				cmp.Transformer("graphql.Input.GoValue", graphql.Input.GoValue))
+			if diff != "" {
+				t.Errorf("Parse(...) (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
